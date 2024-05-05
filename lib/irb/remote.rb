720f50014fba1c94e0c73a127add8720e6c1c7f5
qwyng <ikusawasi@gmail.com>
2024-05-05 16:47:11 +0900

    bundle gem irb-remote -b --no-ext --test=minitest --ci=github --linter=standard

---
+++ lib/irb/remote.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "remote/version"
+
+module Irb
+  module Remote
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/irb/remote/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Irb
+  module Remote
+    VERSION = "0.1.0"
+  end
+end
