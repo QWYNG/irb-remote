e996625876f41885ea2684d3eb24b4464a454d41
qwyng <ikusawasi@gmail.com>
2024-05-21 18:59:11 +0900

    i cannot use refine

--- lib/irb-remote/binding.rb
+++ lib/irb-remote/binding.rb
@@ -8,5 +8,25 @@ module IrbRemote
       IrbRemote::Server.run(self, host, port, options)
     end
     alias remote_irb irb_remote
+
+    def eval_methods
+      ::Kernel.instance_method(:methods).bind(eval('self')).call
+    end
+
+    def eval_private_methods
+      ::Kernel.instance_method(:private_methods).bind(eval('self')).call
+    end
+
+    def eval_instance_variables
+      ::Kernel.instance_method(:instance_variables).bind(eval('self')).call
+    end
+
+    def eval_global_variables
+      ::Kernel.instance_method(:global_variables).bind(eval('self')).call
+    end
+
+    def eval_class_constants
+      ::Module.instance_method(:constants).bind(eval('self.class')).call
+    end
   end
 end
